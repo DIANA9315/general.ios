84f66ede3342057e5982e539562573fe9796d539
Diana Lopez Vega <dianateresalopezvega@gmail.com>
2025-07-22 21:28:31 -0600

    actualizacion

--- FetchII/script.js
+++ FetchII/script.js
@@ -14,24 +14,29 @@ async function obtenerPlatillos(){
 async function agregarPlatillo(){
 
     let nuevoPlatillo = {
-        id: 100,
-        nombre: 'Albóndigas',
-        precio: 100,
+        id: Date.now(),
+        nombre: 'Nombre de platillo',
+        precio: 'Precio del platillo',
         imagen: "https://media.istockphoto.com/id/626752258/photo/albondigas-meatballs-with-tomato-sauce-on-a-plate-close-up.jpg"
     }
 
     let respuesta = await fetch(`${BASE_URL}/platillos`, {
         method: 'POST',
+         headers: { // ¡IMPORTANTE! Añadir Content-Type para JSON
+            'Content-Type': 'application/json'
+         },
         body: JSON.stringify(nuevoPlatillo)
     });
     let json = await respuesta.json();
-    console.log(json);
+    console.log("Platillo agregado",json);
 }
+  obtenerPlatillos();
+
 
 async function eliminarPlatillo(){
     let respuesta = await fetch(`${BASE_URL}/platillos/3`, { method: 'DELETE'} );
     let json = await respuesta.json();
-    console.log(json);
+    console.log("Platillo eliminado",json);
 }
 
 /*
@@ -53,6 +58,10 @@ document.getElementById("btnAgregarPlatillo").addEventListener('click', (e) => {
     console.log("Agregar platillo");
     agregarPlatillo();
 });
+
+
+
+
 
 
 document.getElementById("btnEliminarPlatillo").addEventListener('click', (e) => {
